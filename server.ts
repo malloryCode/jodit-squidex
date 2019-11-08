f3713c3a77a19b908867f38f82ce9b06fd2ec629
smallory <smallory@hodgsonconsult.com>
2019-11-08 15:13:11 -0500

    switched to express server added visual studio support added new example page

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
+import express = require('express');
+
 var app = express();
 var dir = __dirname;
+
 app.use(express.static(dir));
+
 app.listen(1337, function () {
     console.log('Listening on http://localhost:1337/');
 });
-//# sourceMappingURL=server.js.map
+
